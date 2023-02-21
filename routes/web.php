9f302cc283ea67235bc835a2387f1ad5e039a5c2
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-21 13:50:25 +0100

    add controller, model, seeder, migrate and faker

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerTrain as ControllerTrain;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('train_home');
-});
+Route::get('/', [ControllerTrain::class, 'index']);
